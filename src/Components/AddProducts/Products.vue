a4766c0b4222737ddd5b59ddff593370fa9bb7cb
Nelson <nelsonsnake_@hotmail.com>
2019-12-13 13:28:49 -0600

    functional form

--- src/Components/AddProducts/Products.vue
+++ src/Components/AddProducts/Products.vue
@@ -5,39 +5,39 @@
             <div class="form-group">
                 <label class="control-label col-sm-2" for="txtEmpresa">Empresa:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="txtEmpresa" placeholder="Nombre empresa" name="txtEmpresa">
+                    <input type="text" v-model="product.empresa" class="form-control" id="txtEmpresa" placeholder="Nombre empresa" name="txtEmpresa">
                 </div>
             </div>    
             <div class="form-group">
                 <label class="control-label col-sm-2" for="txtTitulo">Titulo del Producto:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="txtTitulo" placeholder="Titulo del producto" name="txtTitulo">
+                    <input type="text" v-model="product.titulo_producto" class="form-control" id="txtTitulo" placeholder="Titulo del producto" name="txtTitulo">
                 </div>
             </div>
             <div class="form-group">
                 <label class="control-label col-sm-2" for="txtDescripcion">Descripcion:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="txtDescripcion" placeholder="Descripcion del producto" name="txtDescripcion">
+                    <input type="text" class="form-control" v-model="product.descripcion" id="txtDescripcion" placeholder="Descripcion del producto" name="txtDescripcion">
                 </div>
             </div>
             <div class="form-group">
                 <label class="control-label col-sm-2" for="txtPrecio">Precio Regular:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="txtPrecio" placeholder="0.0" name="txtPrecio">
+                    <input type="text" class="form-control" v-model="product.precio_regular" id="txtPrecio" placeholder="0.0" name="txtPrecio">
                 </div>
             </div>
             <div class="form-group">
                 <label class="control-label col-sm-2" for="imgFile">Imagen del producto</label>
                 <div class="col-sm-10">
-                    <input type="file" class="form-control" id="imgFile">                
+                    <input type="file" @change="processFile($event)" class="form-control" id="imgFile">                
                 </div>
             </div>
 
              <div class="form-group">
                 <label for="selMoney" class="control-label col-sm-2">Moneda:</label>
                 <div class="col-sm-10">
-                    <select class="form-control" id="selMoney">
-                        <option v-for="option in moneyData" v-bind:key="option.value" >
+                    <select class="form-control" id="selMoney" v-model="product.moneda">
+                        <option v-for="option in moneyData" v-bind:value="option.value" :key="option.value">
                             {{option.name}}
                         </option>
                     </select>
@@ -53,20 +53,35 @@
     </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
     name: "Products",
     data(){
         return {
-            moneyData: [
-                {
-                    value: 'DR',
-                    name : 'Dollar'
-                },
-                {
-                    value: 'LB',
-                    name: 'Libra'
-                }
-            ]
+            moneyData: [],
+            product: {
+                empresa: "",
+                titulo_producto: "",
+                descripcion: "",
+                precio_regular: "",
+                img: "",
+                moneda: "DR"
+            }
+        }
+    },
+    mounted(){
+        axios
+        .get('https://private-e68ed0-nelsongp.apiary-mock.com/moneyTyp')
+        .then(response => {
+            this.moneyData = response.data.moneyData;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    },
+    methods: {
+        processFile(event){
+            this.url = event.target.files[0];
         }
     }
 }
